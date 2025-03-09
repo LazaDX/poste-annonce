bb0cd6fb75060e79493212c5549070b004ff549e
laza <lazanirina2800@gmail.com>
2025-03-09 21:46:17 +0300

    Modification de l'ordre de migration

--- database/migrations/2025_03_09_183511_create_posts_table.php
+++ database/migrations/2025_03_09_183511_create_posts_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->int('title_price');
+            $table->integer('title_price');
             $table->text('description');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
